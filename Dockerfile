7df6730529f45716ad82da236f92587858b5af51
Jorge Ivan Villalba Grandett <jivillalba@softcaribbean.com>
2023-04-02 20:30:14 -0500

    Se dan permisos de CORS y se crea el archivo de Dockefile

---
+++ Dockerfile
@@ -0,0 +1,15 @@
+FROM node:16
+
+WORKDIR /app
+
+COPY package*.json ./
+
+RUN npm install --production
+
+COPY . .
+
+ENV NODE_ENV production
+ENV PORT 3000
+EXPOSE 3000
+
+CMD ["npm", "start"]
--- src/app.js
+++ src/app.js
@@ -1,9 +1,14 @@
 const path = require('path')
 const autoload = require('@fastify/autoload')
+const cors  = require('@fastify/cors');
 
 async function app(fastify, options){
     fastify.register(autoload,{
         dir: path.join(__dirname, 'routes')
-    })
+    });
+    fastify.register(cors, {
+        origin: "*",
+        methods: ["GET","POST","PUT","DELETE"]
+    });
 }
 module.exports = app
